71da4b717f6a1467600c45082c3331af80824442
michega <e-valia@ya.com>
2026-05-29 14:28:52 +0300

    увеличение и сравнение

--- app_4_12operators.js
+++ app_4_12operators.js
@@ -6,6 +6,21 @@ const aria = width*height;
 const newWidth= 5+height;
 const newHeight= 5+width;
 const volum=2**4;
+//4_13
+let age = 15+48
+age+=8;
+age-=58;
+age*=8;
+age/= 2;
+age++;
+age--;
+
+console.log(width>aria);
+console.log(width>=aria);
+console.log(width<aria);
+console.log(width<=aria);
+console.log(width==aria);
+console.log(width===aria);
 
 //strok
 const nameUser='Alice';
